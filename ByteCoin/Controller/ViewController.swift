3146636f1f2a49eaa68e0a2109b456a39c2b7407
Vasilii <shores.freezer0u@icloud.com>
2023-05-06 00:08:34 +1000

    Add PickerView

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 	
+	private let coinManager = CoinManager()
+	
 	private let coinView = CoinView()
-
+	private let pickerView = UIPickerView()
+	
+	// MARK: - Lifecycle
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		pickerView.delegate = self
+		pickerView.dataSource = self
+		
 		addSubviews()
 		constraints()
 		setupView()
@@ -24,17 +33,47 @@ class ViewController: UIViewController {
 private extension ViewController {
 	func addSubviews() {
 		view.addSubview(coinView)
+		view.addSubview(pickerView)
 	}
 	
 	func constraints() {
+		pickerView.translatesAutoresizingMaskIntoConstraints = false
+		
 		NSLayoutConstraint.activate([
 			coinView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
 			coinView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-			coinView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+			coinView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+			
+			pickerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+			pickerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+			pickerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+			
+			pickerView.heightAnchor.constraint(equalToConstant: 216)
 		])
 	}
 	
 	func setupView() {
 		view.backgroundColor = UIColor(named: Color.background)
+	}
+}
+
+// MARK: - PickerView delegate and datasource
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+	func numberOfComponents(in pickerView: UIPickerView) -> Int {
+		return 1
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		return coinManager.currencyArray.count
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		return coinManager.currencyArray[row]
+	}
+	
+	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+		let selectedCurrency = coinManager.currencyArray[row]
+		coinManager.getCoinPrice(for: selectedCurrency)
 	}
 }
---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by VASILY IKONNIKOV on 05.05.2023.
+//
+
+struct CoinManager {
+	
+	let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+	let apiKey = "YOUR_API_KEY_HERE"
+	
+	let currencyArray = ["AUD", "BRL", "CAD", "CNY", "EUR", "GBP", "HKD", "IDR", "ILS", "INR", "JPY", "MXN", "NOK", "NZD", "PLN", "RON", "RUB", "SEK", "SGD", "USD", "ZAR"]
+	
+	func getCoinPrice(for currency: String) {
+		
+	}
+}
