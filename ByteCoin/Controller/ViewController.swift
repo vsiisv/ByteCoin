76e768d26d681e110f81087b68ebf9e07cb9f940
Vasilii <shores.freezer0u@icloud.com>
2023-05-05 12:15:54 +1000

    add support fiels Color

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -11,6 +11,24 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		addSubviews()
+		constraints()
+		setupView()
 	}
 }
 
+// MARK: - Layout
+
+private extension ViewController {
+	func addSubviews() {
+		
+	}
+	
+	func constraints() {
+		
+	}
+	
+	func setupView() {
+		view.backgroundColor = UIColor(named: Color.background)
+	}
+}
---
+++ ByteCoin/Resources/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  ByteCoin
+//
+//  Created by VASILY IKONNIKOV on 05.05.2023.
+//
+
+import Foundation
+
+enum Color {
+	static let background = "Background Color"
+	static let icon = "Icon Color"
+	static let tint = "Title Color"
+}
